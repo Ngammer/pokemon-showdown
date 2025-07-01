a3c6dd68bd33ddcf61488ad3b8180856b3277bd6
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-07-01 13:33:00 -0500

    Nuevo Meta NFE

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -8,17 +8,29 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		column: 1,
 	},
 	{
+		name: "[Gen 9] Nuevo Meta Random Battle",
+		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
+		mod: 'gen9',
+		team: 'randomBSSFactory',
+		ruleset: ['Standard NatDex', 'PotD', 'Illusion Level Mod', 'Nuevo Meta Pokedex', 'Terastal Clause', 'Item Clause = 1'],
+	},
+	{
 		name: "[Gen 9] Nuevo Meta",
 		mod: 'gen9',
 		ruleset: ['Standard NatDex', 'Terastal Clause', 'Nuevo Meta Pokedex', '!Evasion Clause', '!OHKO Clause'],
 		banlist: ['Baton Pass', 'Uber'],
 	},
 	{
-		name: "[Gen 9] Nuevo Meta Random Battle",
-		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
+		name: "[Gen 9] Nuevo Meta NFE",
+		desc: `Only Pok&eacute;mon that can evolve are allowed.`,
 		mod: 'gen9',
-		team: 'randomBSSFactory',
-		ruleset: ['Standard NatDex', 'PotD', 'Illusion Level Mod', 'Nuevo Meta Pokedex', 'Terastal Clause', 'Item Clause = 1'],
+		searchShow: false,
+		ruleset: ['Not Fully Evolved', 'Standard NatDex', 'Terastal Clause', 'Nuevo Meta Pokedex', '!Evasion Clause', '!OHKO Clause'],
+		banlist: [
+			'Basculin-White-Striped', 'Bisharp', 'Chansey', 'Combusken', 'Dipplin', 'Duraludon', 'Electabuzz', 'Gligar', 'Gurdurr',
+			'Haunter', 'Magmar', 'Magneton', 'Misdreavus', 'Porygon2', 'Primeape', 'Qwilfish-Hisui', 'Rhydon', 'Scyther', 'Sneasel',
+			'Sneasel-Hisui', 'Ursaring', 'Vulpix-Base', 'Arena Trap', 'Magnet Pull', 'Moody', 'Shadow Tag', 'Baton Pass',
+		],
 	},
 	{
 		name: "[Gen 9] Nuevo Meta LC",
