5345fb12114618eda17a6442ba53b39c165634bf
Ngammer <cnicolas810@gmail.com>
2025-07-01 15:27:07 -0300

    abra

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -25,7 +25,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		mod: 'gen9',
 		ruleset: ['Little Cup', 'Standard NatDex', '!Evasion Clause', '!OHKO Clause', 'Terastal Clause', 'Nuevo Meta Pokedex'],
 		banlist: [
-			'Aipom', 'Azurill', 'Bonsly', 'Corsola-Galar', 'Farfetch\u2019d-Galar', 'Girafarig', 'Gligar', 'Happiny', 'Houndour', 'Lickitung', 'Mantyke', 'Mime Jr.', 'Misdreavus',
+			'Aipom', 'Abra', 'Azurill', 'Bonsly', 'Corsola-Galar', 'Farfetch\u2019d-Galar', 'Girafarig', 'Gligar', 'Happiny', 'Lickitung', 'Mantyke', 'Mime Jr.', 'Misdreavus',
 			'Munchlax', 'Murkrow', 'Qwilfish-Hisui', 'Scyther', 'Smoochum', 'Sneasel', 'Sneasel-Hisui', 'Tangela', 'Vulpix-Base', 'Wynaut', 'Yanma', 'Splash', 'Dream Ball',
 			 'Heat Rock', 'Oval Stone', 'Baton Pass', 'Moody',
 		],
