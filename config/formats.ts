790b7e6245052a1f8ea03fb64be90a51f7eb3aef
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-07-01 13:23:52 -0500

    orden

--- config/formats.ts
+++ config/formats.ts
@@ -110,8 +110,8 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['Little Cup', 'Standard'],
 		banlist: [
 			'Aipom', 'Azurill', 'Bonsly', 'Corsola-Galar', 'Farfetch\u2019d-Galar', 'Girafarig', 'Gligar', 'Happiny', 'Houndour', 'Lickitung', 'Mantyke', 'Mime Jr.', 'Misdreavus',
-			'Munchlax', 'Murkrow', 'Qwilfish-Hisui', 'Scyther', 'Smoochum', 'Sneasel', 'Sneasel-Hisui', 'Tangela', 'Vulpix-Base', 'Wynaut', 'Yanma', 'Splash', 'Dream Ball',
-			 'Heat Rock', 'Oval Stone', 'Baton Pass', 'Moody',
+			'Munchlax', 'Murkrow', 'Qwilfish-Hisui', 'Scyther', 'Smoochum', 'Sneasel', 'Sneasel-Hisui', 'Tangela', 'Vulpix-Base', 'Wynaut', 'Yanma', 'Dream Ball',
+			 'Heat Rock', 'Oval Stone', 'Baton Pass', 'Splash', 'Moody',
 		],
 	},
 	{
