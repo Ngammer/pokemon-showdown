06283285d3045c0a0d0b152f29ba7aec80b64b4c
Ngammer <cnicolas810@gmail.com>
2025-07-08 12:04:00 -0300

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -24,12 +24,11 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] Nuevo Meta NFE",
 		desc: `Only Pok&eacute;mon that can evolve are allowed.`,
 		mod: 'gen9',
-		searchShow: false,
 		ruleset: ['Not Fully Evolved', 'Standard NatDex', 'Terastal Clause', 'Nuevo Meta Pokedex', '!Evasion Clause', '!OHKO Clause'],
 		banlist: [
 			'Aipom', 'Azurill', 'Bonsly', 'Corsola-Galar', 'Electabuzz', 'Farfetch\u2019d-Galar', 'Girafarig', 'Gligar', 'Happiny', 'Lickitung', 'Magmar', 'Magneton', 'Mantyke', 'Mime Jr.',
-			'Misdreavus', 'Mr. Mime-Galar', 'Munchlax', 'Murkrow', 'Piloswine', 'Porygon2', 'Primeape', 'Qwilfish-Hisui', 'Rhydon', 'Sneasel', 'Sneasel-Hisui', 'Tangela', 'Togetic',
-			'Ursaring', 'Wynaut', 'Yanma', 'Oval Stone', 'Baton Pass',
+			'Misdreavus', 'Mr. Mime-Galar', 'Munchlax', 'Murkrow', 'Piloswine', 'Porygon2', 'Primeape', 'Qwilfish-Hisui', 'Rhydon', 'Scyther', 'Sneasel', 'Sneasel-Hisui', 'Tangela', 'Togetic',
+			'Ursaring', 'Wynaut', 'Yanma', 'Heat Rock', 'Light Ball', 'Oval Stone', 'Baton Pass',
 		],
 	},
 	{
