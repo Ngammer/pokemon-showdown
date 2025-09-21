420092c5471a0805dc9b3d2a958c78379baf8a18
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-09-21 18:08:47 -0500

    Algunos movs

--- data/moves.ts
+++ data/moves.ts
@@ -1173,9 +1173,9 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	beatup: {
 		num: 251,
 		accuracy: 100,
-		basePower: 100,
+		basePower: 110,
 		basePowerCallback(pokemon, target, move) {
-			return 100 - 5 * pokemon.side.totalFainted;
+			return 110 - 15 * pokemon.side.totalFainted;
 		},
 		category: "Physical",
 		name: "Beat Up",
@@ -1263,24 +1263,31 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	bestow: {
 		num: 516,
 		accuracy: true,
-		basePower: 0,
-		category: "Status",
-
+		basePower: 90,
+		category: "Special",
 		name: "Bestow",
 		pp: 15,
 		priority: 0,
 		flags: { mirror: 1, bypasssub: 1, allyanim: 1, noassist: 1, failcopycat: 1 },
+		onTryHit(target, source, move) {
+			if (source.isAlly(target)) {
+				move.basePower = 0;
+				move.infiltrates = true;
+			}
+		},
+		onTryMove(source, target, move) {
+			if (source.isAlly(target) && source.volatiles['healblock']) {
+				this.attrLastMove('[still]');
+				this.add('cant', source, 'move: Heal Block', move);
+				return false;
+			}
+		},
 		onHit(target, source, move) {
-			if (target.item) {
-				return false;
+			if (source.isAlly(target)) {
+				if (!this.heal(Math.floor(target.baseMaxhp * 0.5))) {
+					return this.NOT_FAIL;
+				}
 			}
-			const myItem = source.takeItem();
-			if (!myItem) return false;
-			if (!this.singleEvent('TakeItem', myItem, source.itemState, target, source, move, myItem) || !target.setItem(myItem)) {
-				source.item = myItem.id;
-				return false;
-			}
-			this.add('-item', target, myItem.name, '[from] move: Bestow', `[of] ${source}`);
 		},
 		secondary: null,
 		target: "normal",
@@ -1647,7 +1654,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	boneclub: {
 		num: 125,
 		accuracy: 100,
-		basePower: 80,
+		basePower: 90,
 		category: "Physical",
 
 		name: "Bone Club",
@@ -1786,12 +1793,17 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 15,
 		priority: 0,
 		flags: { contact: 1, protect: 1, mirror: 1 },
-		secondary: {
-			chance: 100,
-			boosts: {
-				atk: -1,
+		secondaries: [
+			{
+				chance: 100,
+				boosts: {
+					atk: -1,
+				},
+			}, {
+				chance: 30,
+				volatileStatus: 'flinch',
 			},
-		},
+		],
 		target: "allAdjacentFoes",
 		type: "Dragon",
 	},
@@ -1814,7 +1826,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	brickbreak: {
 		num: 280,
 		accuracy: 100,
-		basePower: 75,
+		basePower: 80,
 		category: "Physical",
 		name: "Brick Break",
 		pp: 15,
@@ -1870,12 +1882,17 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 20,
 		priority: 0,
 		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
-		secondary: {
-			chance: 100,
-			boosts: {
-				atk: -1,
+		secondaries: [
+			{
+				chance: 100,
+				boosts: {
+					atk: -1,
+				},
+			}, {
+				chance: 30,
+				volatileStatus: 'confusion',
 			},
-		},
+		],
 		target: "allAdjacent",
 		type: "Dark",
 		contestType: "Tough",
@@ -1903,7 +1920,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	bubblebeam: {
 		num: 61,
 		accuracy: 100,
-		basePower: 70,
+		basePower: 85,
 		category: "Special",
 		name: "Bubble Beam",
 		pp: 20,
@@ -1927,7 +1944,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "Bug Bite",
 		pp: 20,
 		priority: 0,
-		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1, bite: 1, },
 		onHit(target, source, move) {
 			const item = target.getItem();
 			if (source.hp && item.isBerry && target.takeItem(source)) {
@@ -1991,6 +2008,12 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 20,
 		priority: 0,
 		flags: { protect: 1, mirror: 1, nonsky: 1, metronome: 1 },
+		onTryHit(pokemon) {
+			// will shatter screens through sub, before you hit
+			pokemon.side.removeSideCondition('spikes');
+			pokemon.side.removeSideCondition('toxicspikes');
+			pokemon.side.removeSideCondition('stealthrock');
+		},
 		secondary: {
 			chance: 100,
 			boosts: {
@@ -2221,6 +2244,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		},
 		boosts: {
 			spa: -6,
+			atk: -6,
 		},
 		secondary: null,
 		target: "allAdjacentFoes",
@@ -2351,7 +2375,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	chargebeam: {
 		num: 451,
 		accuracy: 100,
-		basePower: 40,
+		basePower: 50,
 		category: "Special",
 		name: "Charge Beam",
 		pp: 10,
@@ -2376,10 +2400,10 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		category: "Status",
 		name: "Charm",
 		pp: 20,
-		priority: 0,
+		priority: 1,
 		flags: { protect: 1, reflectable: 1, mirror: 1, allyanim: 1, metronome: 1 },
 		boosts: {
-			atk: -2,
+			spa: -2,
 		},
 		secondary: null,
 		target: "normal",
