1314f2d5ace7bd726cf9231661ae54ea0e740744
Ngammer <cnicolas810@gmail.com>
2026-02-10 17:34:48 -0300

    Update start-pages.bat

--- start-pages.bat
+++ start-pages.bat
@@ -1,4 +1,5 @@
 start cmd /k node D:\Documents\IC\pokemon\pokemon-showdown\pokemon-showdown
 start cmd /k npx serve -l 3000 D:\Documents\IC\pokemon\pokemon-showdown-client\play.pokemonshowdown.com
 start cmd /k npx serve -l 3001 D:\Documents\IC\pokemon\pokemon-showdown-client\pokemonshowdown.com
+start cmd /k npx serve -l 3002 D:\Documents\IC\pokemon\pokemon-showdown-client\replay.pokemonshowdown.com
 start cmd /k npx serve -l 3003 D:\Documents\IC\pokemon\Pokemon-Showdown-Dex
