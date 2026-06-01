6d47d604c6a0eeaac96368f4ca60834fccc89081
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2026-05-31 19:55:26 -0500

    Solo bichos

---
+++ data/mods/gen9bugdex/formats-data.ts
@@ -0,0 +1,364 @@
+export const FormatsData: import('../../../sim/dex-species').SpeciesFormatsDataTable = {
+	caterpie: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	metapod: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	butterfree: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	weedle: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	kakuna: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	beedrill: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	beedrillmega: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	paras: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	parasect: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	venonat: {
+		natDexTier: "OU",
+	},
+	venomoth: {
+		natDexTier: "OU",
+	},
+	scyther: {
+		natDexTier: "OU",
+	},
+	scizor: {
+		natDexTier: "OU",
+	},
+	scizormega: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	kleavor: {
+		natDexTier: "OU",
+	},
+	pinsir: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	pinsirmega: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	ledyba: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	ledian: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	spinarak: {
+		natDexTier: "OU",
+	},
+	ariados: {
+		natDexTier: "OU",
+	},
+	yanma: {
+		natDexTier: "OU",
+	},
+	yanmega: {
+		natDexTier: "OU",
+	},
+	pineco: {
+		natDexTier: "OU",
+	},
+	forretress: {
+		natDexTier: "OU",
+	},
+	shuckle: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	heracross: {
+		natDexTier: "OU",
+	},
+	heracrossmega: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	wurmple: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	silcoon: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	beautifly: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	cascoon: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	dustox: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	surskit: {
+		natDexTier: "OU",
+	},
+	masquerain: {
+		natDexTier: "OU",
+	},
+	nincada: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	ninjask: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	shedinja: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	volbeat: {
+		natDexTier: "OU",
+	},
+	illumise: {
+		natDexTier: "OU",
+	},
+	anorith: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	armaldo: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	kricketot: {
+		natDexTier: "OU",
+	},
+	kricketune: {
+		natDexTier: "OU",
+	},
+	burmy: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	wormadam: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	wormadamsandy: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	wormadamtrash: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	mothim: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	combee: {
+		natDexTier: "OU",
+	},
+	vespiquen: {
+		natDexTier: "OU",
+	},
+	skorupi: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	sewaddle: {
+		natDexTier: "OU",
+	},
+	swadloon: {
+		natDexTier: "OU",
+	},
+	leavanny: {
+		natDexTier: "OU",
+	},
+	venipede: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	whirlipede: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	scolipede: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	dwebble: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	crustle: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	karrablast: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	escavalier: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	joltik: {
+		natDexTier: "OU",
+	},
+	galvantula: {
+		natDexTier: "OU",
+	},
+	shelmet: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	accelgor: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	durant: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	larvesta: {
+		natDexTier: "OU",
+	},
+	volcarona: {
+		natDexTier: "OU",
+	},
+	genesect: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	genesectburn: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	genesectchill: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	genesectdouse: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	genesectshock: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	scatterbug: {
+		natDexTier: "OU",
+	},
+	spewpa: {
+		natDexTier: "OU",
+	},
+	vivillon: {
+		natDexTier: "OU",
+	},
+	grubbin: {
+		natDexTier: "OU",
+	},
+	charjabug: {
+		natDexTier: "OU",
+	},
+	vikavolt: {
+		natDexTier: "OU",
+	},
+	cutiefly: {
+		natDexTier: "OU",
+	},
+	ribombee: {
+		natDexTier: "OU",
+	},
+	dewpider: {
+		natDexTier: "OU",
+	},
+	araquanid: {
+		natDexTier: "OU",
+	},
+	wimpod: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	golisopod: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	buzzwole: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	pheromosa: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	blipbug: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	dottler: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	orbeetle: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	sizzlipede: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	centiskorch: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
+	snom: {
+		natDexTier: "OU",
+	},
+	frosmoth: {
+		natDexTier: "OU",
+	},
+	tarountula: {
+		natDexTier: "OU",
+	},
+	spidops: {
+		natDexTier: "OU",
+	},
+	nymble: {
+		natDexTier: "OU",
+	},
+	lokix: {
+		natDexTier: "OU",
+	},
+	rellor: {
+		natDexTier: "OU",
+	},
+	rabsca: {
+		natDexTier: "OU",
+	},
+	slitherwing: {
+		natDexTier: "OU",
+	},
+};
