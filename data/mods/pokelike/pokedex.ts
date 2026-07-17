92aca7f4f6fa60b2fa1c67b07a340efefcf6503e
Ngammer <cnicolas810@gmail.com>
2026-07-17 12:45:43 -0300

    base

--- data/mods/pokelike/pokedex.ts
+++ data/mods/pokelike/pokedex.ts
@@ -1,6 +1,212 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	// Adri
 	marowak: {
 		inherit: true,
 		abilities: { 0: "Rock Head", 1: "Sand Force", H: "Battle Armor", S: "Limber"/* , S2: "Reckless", S3: "Unburden" */ },
+	},
+	zebstrika: {
+		inherit: true,
+	},
+	latias: {
+		inherit: true,
+	},
+	latiasmega: {
+		inherit: true,
+		abilities: { 0: "Levitate" },
+		requiredItem: undefined,
+	},
+	dodrio: {
+		inherit: true,
+	},
+	simipour: {
+		inherit: true,
+	},
+	glimmora: {
+		inherit: true,
+	},
+	glimmoramega: {
+		inherit: true,
+	},
+	// Johan
+	weezing: {
+		inherit: true,
+	},
+	overqwil: {
+		inherit: true,
+	},
+	furrip: {
+		inherit: true,
+	},
+	dragapult: {
+		inherit: true,
+	},
+	dragapultmega: {
+		inherit: true,
+	},
+	chatot: {
+		inherit: true,
+	},
+	umbreon: {
+		inherit: true,
+	},
+	// Sebas
+	kingler: {
+		inherit: true,
+	},
+	kinglermega: {
+		inherit: true,
+	},
+	torterra: {
+		inherit: true,
+	},
+	crobat: {
+		inherit: true,
+	},
+	spidops: {
+		inherit: true,
+	},
+	cattmory: {
+		inherit: true,
+	},
+	ironthorns: {
+		inherit: true,
+	},
+	// Zape
+	dragalge: {
+		inherit: true,
+	},
+	dragalgemega: {
+		inherit: true,
+	},
+	parasect: {
+		inherit: true,
+	},
+	mienshao: {
+		inherit: true,
+	},
+	kangaskhan: {
+		inherit: true,
+	},
+	kangaskhanmega: {
+		inherit: true,
+	},
+	tinkaton: {
+		inherit: true,
+	},
+	masquerain: {
+		inherit: true,
+	},
+	// Gerr
+	barbaracle: {
+		inherit: true,
+	},
+	barbaraclemega: {
+		inherit: true,
+	},
+	dugtrio: {
+		inherit: true,
+	},
+	luxray: {
+		inherit: true,
+	},
+	gastrodon: {
+		inherit: true,
+	},
+	breloom: {
+		inherit: true,
+	},
+	blacephalon: {
+		inherit: true,
+	},
+	// Andlol
+	minior: {
+		inherit: true,
+	},
+	skeledirge: {
+		inherit: true,
+	},
+	naganadel: {
+		inherit: true,
+	},
+	xatu: {
+		inherit: true,
+	},
+	xatutotem: {
+		inherit: true,
+	},
+	scolipede: {
+		inherit: true,
+	},
+	scolipedemega: {
+		inherit: true,
+	},
+	purugly: {
+		inherit: true,
+	},
+	// Pou
+	slowbrogalar: {
+		inherit: true,
+	},
+	shiftry: {
+		inherit: true,
+	},
+	scizor: {
+		inherit: true,
+	},
+	scizormega: {
+		inherit: true,
+	},
+	mamoswine: {
+		inherit: true,
+	},
+	kricketune: {
+		inherit: true,
+	},
+	frosmoth: {
+		inherit: true,
+	},
+	// Fabio
+	aerodactyl: {
+		inherit: true,
+	},
+	aerodactylmega: {
+		inherit: true,
+	},
+	golurk: {
+		inherit: true,
+	},
+	toxtricitylowkey: {
+		inherit: true,
+	},
+	goodrahisui: {
+		inherit: true,
+	},
+	dachsbun: {
+		inherit: true,
+	},
+	venomoth: {
+		inherit: true,
+	},
+	// Sam
+	walrein: {
+		inherit: true,
+	},
+	sceptile: {
+		inherit: true,
+	},
+	disdowoodion: {
+		inherit: true,
+	},
+	palafin: {
+		inherit: true,
+	},
+	palafinhero: {
+		inherit: true,
+	},
+	lumineonhoenn: {
+		inherit: true,
+	},
+	nidoqueen: {
+		inherit: true,
 	},
 };
