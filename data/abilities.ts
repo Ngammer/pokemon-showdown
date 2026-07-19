03738b6b4c31cad9a4e51fa3c83c051fa9380e3a
Ngammer <cnicolas810@gmail.com>
2026-07-18 23:37:56 -0300

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -8301,6 +8301,27 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		flags: { },
 		name: "Emergent Excavation",
 		rating: 4,
-		num: -198,
+		num: -199,
+	},
+	soaked: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && (!noModifyType.includes(move.id) || this.activeMove?.isMax) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Water';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		flags: { },
+		name: "Soaked",
+		rating: 4,
+		num: -200,
 	},
 };
