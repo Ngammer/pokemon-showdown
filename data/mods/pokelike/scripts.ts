066f26b26acf75129a18a1f85d96be1919d492c1
Ngammer <cnicolas810@gmail.com>
2026-07-23 20:10:14 -0300

    Update scripts.ts

--- data/mods/pokelike/scripts.ts
+++ data/mods/pokelike/scripts.ts
@@ -1,3 +1,17 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
+	init() {
+		this.modData("Learnsets", "barbaracle").learnset.tropkick = ["9L1"];
+		this.modData("Learnsets", "gastrodon").learnset.strengthsap = ["9L1"];
+		this.modData("Learnsets", "breloom").learnset.combattorque = ["9L1"];
+		this.modData("Learnsets", "blacephalon").learnset.bitterblade = ["9L1"];
+		this.modData("Learnsets", "chesnaught").learnset.kalostrio = ["9L1"];
+		this.modData("Learnsets", "skeledirge").learnset.astralbarrage = ["9L1"];
+		this.modData("Learnsets", "scolipede").learnset.direclaw = ["9L1"];
+		this.modData("Learnsets", "slowbrogalar").learnset.luminacrash = ["9L1"];
+		this.modData("Learnsets", "shiftry").learnset.wickedblow = ["9L1"];
+		this.modData("Learnsets", "sceptile").learnset.spicyextract = ["9L1"];
+		this.modData("Learnsets", "qwilfish").learnset.fishiousrend = ["9L1"];
+		this.modData("Learnsets", "regigigas").learnset.behemothblade = ["9L1"];
+	},
 };
